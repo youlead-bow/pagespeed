513bbe7f409ae394bbeafa1e94e26f9becb588d1
Flavien Bucheton <f.bucheton@youlead.fr>
2023-09-28 16:52:37 +0200

    project initialisation

---
+++ src/PageSpeed/Insights/Service.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class Service
+{
+    public function __construct(
+        private readonly HttpClientInterface $client,
+        private readonly string $apiUrl,
+        private readonly string $apiKey,
+        private string $locales = 'fr_Fr',
+        private int $timeout = 300
+    )
+    {
+    }
+
+    public function setLocales(string $locales): static
+    {
+        $this->locales = $locales;
+
+        return $this;
+    }
+
+    public function setTimeout(int $timeout): static
+    {
+        $this->timeout = $timeout;
+
+        return $this;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    private function getRequest(string $url, string $strategy, array $extraParams = []): array
+    {
+        $params = array_merge([
+            'locale' => $this->locales,
+            'key' => $this->apiKey,
+            'url' => $url,
+            'strategy' => $strategy
+        ], $extraParams);
+
+        $response = $this->client->request('GET', $this->apiUrl, [
+            'query' => $params,
+            'timeout' => $this->timeout
+        ]);
+
+        return $response->toArray();
+    }
+}
